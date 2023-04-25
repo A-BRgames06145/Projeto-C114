dcc6dde245f29af169f6281b21f47a0c9e392437
A-BRgames06145 <93745799+A-BRgames06145@users.noreply.github.com>
2023-04-25 20:42:32 -0300

    Add files via upload

---
+++ Projeto C114/teste1.py
@@ -0,0 +1,9 @@
+a = 33
+b = 33
+
+if b > a:
+    print("b é maior que a")
+elif b == a:
+    print("b e a são iguais")
+
+# Resultado: b e a são iguais
---
+++ Projeto C114/teste2.py
@@ -0,0 +1,13 @@
+def my_function(fname):
+    print(fname + " Batata")
+
+my_function("Emily")
+my_function("Tobias")
+my_function("Lucas")
+my_function("Duque")
+
+# Resultado: 
+# Emily Batata
+# Tobias Batata
+# Lucas Batata
+# Duque Batata
